3f45b3e70bb272902334775a8df5d40a1d8a7821
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-03-17 10:54:35 -0400

    added links to menu items by adding pages folderand appropriate .js files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
 import Services from './pages/services';
+import Contact from './pages/contact';
+import Signup from './pages/signup';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
         <Route path="/services" exact component={Services} />
+        <Route path="/contact-us" exact component={Contact} />
+        <Route path="/sign-up" exact component={Signup} />
       </Switch>
     </Router>
   );
